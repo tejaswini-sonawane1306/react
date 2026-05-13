16d29a5474731a51275913ab43b237b9081cdf43
tejaswini-sonawane1306 <sonawanetejaswini2304@gmail.com>
2026-05-13 15:25:20 +0530

    react done

---
+++ app.js
@@ -0,0 +1,4 @@
+  const arr = [10,20,30,40,50]
+  export const user = 'teja'
+
+  export default arr
---
+++ script.js
@@ -0,0 +1,5 @@
+import a from './app.js'
+import {user} from './app.js'
+
+ console.log(a)
+ console.log(user)
